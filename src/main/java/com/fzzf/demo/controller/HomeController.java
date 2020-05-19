6d5bbe66727958a907b949d75bceb0462ac71011
luchangwei <170710>
2020-05-20 00:35:38 +0800

    update

---
+++ src/main/java/com/fzzf/demo/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.fzzf.demo.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping("/index")
+    public String index() {
+        return "index";
+    }
+}
---
+++ src/test/java/com/fzzf/demo/HomeTests.java
@@ -0,0 +1,31 @@
+package com.fzzf.demo;
+
+import com.fzzf.demo.controller.HomeController;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HomeTests {
+
+    private MockMvc mvc;
+
+    @Before
+    public void setUp() throws Exception {
+        mvc = MockMvcBuilders.standaloneSetup(new HomeController()).build();
+    }
+
+    @Test
+    public void getHello() throws Exception {
+//        mvc.perform(MockMvcRequestBuilders.get("/index").accept(MediaType.APPLICATION_JSON))
+//                .andExpect(status().isOk())
+//                .andExpect(content().string(equalTo("Hello World")));
+    }
+}
